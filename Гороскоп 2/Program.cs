03b84b9658dd1818d7fe6cd76400defe27c104d0
Aletkina <115992868+Aletkina@users.noreply.github.com>
2022-11-28 13:13:39 +0300

    Add files via upload

---
+++ Гороскоп 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Алёткина_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name;
+            string surname;
+            string animal;
+            string sign;
+            System.Console.WriteLine("Введите ваше имя: ");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Введите вашу фамилию: ");
+            surname = System.Console.ReadLine();
+            System.Console.WriteLine("Введите ваше любимое животное: ");
+            animal = System.Console.ReadLine();
+            System.Console.WriteLine("Введите ваш знак задиака: ");
+            sign = System.Console.ReadLine();
+            System.Console.WriteLine($"Индивидуальный гороскоп для пользователя {name} {surname}");
+            System.Console.WriteLine($"Кем вы были в прошлой жизни: {animal}");
+            System.Console.WriteLine($"Ваш знак задиака - {sign} ,поэтому вы - тонко чувствующая натура");
+            System.Console.WriteLine("Для завершения сеанса нажмите ENTER.");
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
